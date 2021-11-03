68c91d57a648557240e4ea5e9cfcd7e1214b5b2d
Liam <11602015+lgvacanti@users.noreply.github.com>
2021-11-03 21:40:27 +0100

    Add cloud run

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,49 @@ resource "google_cloudbuild_trigger" "trigger" {
   filename = "buildtemplate.yaml"
 }
 
+data "google_project" "project" {
+
+}
+
+resource "google_project_iam_member" "cloudbuild_roles" {
+  for_each = toset(["roles/run.admin", "roles/iam.serviceAccountUser"])
+  project  = var.project
+  role     = each.key
+  member   = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
+
+resource "google_cloud_run_service" "cloud_run" {
+  name     = "container"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+}
+
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+resource "google_cloud_run_service_iam_policy" "policy" {
+  location    = "us-central1"
+  project     = var.project
+  service     = google_cloud_run_service.cloud_run.name
+  policy_data = data.google_iam_policy.admin.policy_data
+}
+
 output "repo-url" {
   value = google_sourcerepo_repository.repo.url
+}
+
+output "app-url" {
+  value = google_cloud_run_service.cloud_run.status[0].url
 }
