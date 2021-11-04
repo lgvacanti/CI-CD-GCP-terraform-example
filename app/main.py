4ca25b3f0b46db93556b51e2cbd5633fec46f77c
Liam <11602015+lgvacanti@users.noreply.github.com>
2021-11-04 14:54:21 +0100

    Add tests

--- app/main.py
+++ app/main.py
@@ -10,6 +10,10 @@ def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
 
+def some_function(a):
+    return a + 1
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
---
+++ app/test.py
@@ -0,0 +1,4 @@
+from main import some_function
+
+def test_function():
+    assert some_function(4) == 5
